7bf057ca9fd948381ece2f362ae738be01a504dd
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-11-07 22:56:42 -0700

    one more passed! 11 total

--- myReduce.js
+++ myReduce.js
@@ -1,26 +1,33 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce for more details
 // Don't worry about initialValue at first. You can always add it in later.
 
-function myReduce(arr, callback) {
+function myReduce(arr, callback, initialValue) {
 //console.log("myReduce");
 //  CODE INSIDE HERE   //
 var out;
-
-for (var i = 1; i < arr.length; i++) {
-	var prev=arr[i-1];
-	var curr=arr[i];
-	out =callback(prev,curr,i,arr);
-	console.log("previous"+prev);
-	console.log("current"+curr);
-	console.log("i"+i);
-	console.log("arr"+arr);
-//	console.log(x);
-	//console.log(y);
-
+var prev;
+if(initialValue>=0 && arr.length===0){
+	return initialValue;
 }
+else if(initialValue>=0){
+start = 0;
+prev= initialValue;
+}
+
+else{
+	start = 1;
+	prev = arr[i-1];
+}
+
+
+for (var i = start; i < arr.length; i++) {
+	curr = arr[i];
+	out=callback(prev,curr,i,arr);
+	}
 return out;
-}
 
+
+}
 /*
  Best if you don't code out here.
  If you want to check your code, use `index.js`!
